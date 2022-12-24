4c5195ea5d43f99be778afed8368d0cddd1c4cae
marcos-dev88 <mv.piesske@gmail.com>
2022-12-23 23:23:09 -0300

    installing lsp plugins

--- lua/plugins/plugin-pack.lua
+++ lua/plugins/plugin-pack.lua
@@ -79,7 +79,7 @@ return require('packer').startup(function()
         requires = { {'nvim-lua/plenary.nvim'} },
         require("telescope").setup{
             defaults = {
-                prompt_prefix = "⇻ ",
+                prompt_prefix = "✖ ",
                 selection_caret = "⇢ ",
                 path_display = { "smart" },
             }       
@@ -130,6 +130,22 @@ return require('packer').startup(function()
   
     -- Def colorscheme
     require('palenightfall').setup()     
+
+    -- LSP config
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'L3MON4D3/LuaSnip'
+    use 'rafamadriz/friendly-snippets'
+    use 'ray-x/lsp_signature.nvim'
+    use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+    use 'onsails/lspkind-nvim'
+
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
