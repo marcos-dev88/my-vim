07cc6935a3c3c95e7605f716264490eb73a49710
marcos-dev88 <mv.piesske@gmail.com>
2022-12-26 22:01:06 -0300

    fix gopls problems

--- lua/plugins/lsp/config/golang/init.lua
+++ lua/plugins/lsp/config/golang/init.lua
@@ -1,6 +1,9 @@
 nvim_lsp = require "lspconfig"
+
+local my_go_bin = os.getenv("GOBIN")
+
   nvim_lsp.gopls.setup {
-    cmd = {"gopls", "serve"},
+    cmd = { my_go_bin .. "/gopls", "serve" },
     settings = {
       gopls = {
         analyses = {
