b81cc0e9fd640a7ec869b8f6f0c45ea979c6f485
marcos-dev88 <mv.piesske@gmail.com>
2023-03-16 21:51:38 -0300

    updating colors and moving icons to lua/plugins/lsp/diagnostics

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@ local cmd = vim.cmd
 require("config.vim_conf")
 require("config.maps")
 require("plugins")
-require("config.lsp_icons")
 
 cmd([[
     :colorscheme purify
-    :hi NormalFloat ctermfg=White
+    :hi NormalFloat ctermfg=White ctermbg=233 guifg=#3a3a3a "rbg=58,58,58
 ]])
+
