6892572a67943855e04a6eced6b8a9a7b617c908
MarcosP <marcos.vp.dev88@gmail.com>
2023-11-06 21:30:59 -0300

    creating new way to replacement

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -43,3 +43,17 @@ keymap('n', '<leader>s', "<cmd>Telescope live_grep_args<CR>", noremap_opts)
 vim.keymap.set('n', 'ff', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+vim.keymap.set('n', 'fr', function()
+    local in_search = vim.fn.input("Find > ")
+    local in_replace = vim.fn.input("Replace > ")
+
+    vim.cmd(string.format([[ 
+        :vimgrep /%s/gj **/* 
+        set grepprg=rg\ --vimgrep\ --no-heading\ --smart-case
+        %s
+        :cfdo %s/%s/%s/gce | update ]], 
+        in_search, "set grepformat=%f:%l:%c:%m", "%s", in_search, in_search
+    ))
+ 
+end)
