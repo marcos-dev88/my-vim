a63abb306bf59ffad033c019a1b7a8ad63ffedf9
MarcosP <marcos.vp.dev88@gmail.com>
2026-05-07 16:14:59 -0300

    feat: update js/ts support lsp
    Signed-off-by: MarcosP <marcos.vp.dev88@gmail.com>

--- lua/plugins/lsp/config/js/init.lua
+++ lua/plugins/lsp/config/js/init.lua
@@ -3,57 +3,45 @@ if pcall(require, "cmp_nvim_lsp") then
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 end
 
-vim.lsp.config("eslint", {
-    capabilities = capabilities,
-    root_dir = function(fname)
-        return vim.fs.root(fname, {
-            ".eslintrc", ".eslintrc.js", ".eslintrc.json", "package.json", ".git"
-        })
-    end,
-    on_attach = function(client, bufnr)
-        client.server_capabilities.completionProvider = nil
-        client.server_capabilities.hoverProvider = false
-
-        local group = vim.api.nvim_create_augroup("EslintFix", { clear = true })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = group,
-            buffer = bufnr,
-            command = "EslintFixAll",
-        })
-    end,
-})
-
-vim.lsp.config("ts_ls", {
-    cmd = { "typescript-language-server", "--stdio" },
-    capabilities = capabilities,
-    root_dir = function(fname)
-        return vim.fs.root(fname, {
-            "tsconfig.json", "jsconfig.json", "package.json", ".git"
-        })
-    end,
-    on_attach = function(client)
-        client.server_capabilities.documentFormattingProvider = false
-    end,
-    settings = {
-        typescript = {
-            inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
+vim.lsp.config("vtsls", {
+    install = {
+        cmd = { "vtsls", "--stdio" },
+    },
+    default_config = {
+        cmd = { "vtsls", "--stdio" },
+        capabilities = capabilities,
+        root_dir = function(fname)
+            return vim.fs.root(fname, { "tsconfig.json", "jsconfig.json", "package.json", ".git" })
+        end,
+        settings = {
+            vtsls = {
+                autoUseWorkspaceTsdk = true,
+                experimental = { completion = { enableServerSideFuzzyMatch = true } },
+            },
+            typescript = {
+                updateImportsOnFileMove = { enabled = "always" },
+                suggest = { completeFunctionCalls = true },
+                inlayHints = {
+                    parameterNames = { enabled = "all" },
+                    parameterTypes = { enabled = true },
+                    variableTypes = { enabled = true },
+                    functionLikeReturnTypes = { enabled = true },
+                },
             },
         },
-        javascript = {
-            inlayHints = { includeInlayParameterNameHints = "all" },
-        },
-    },
+        on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+            
+            if client.server_capabilities.inlayHintProvider then
+                vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+            end
+        end,
+    }
 })
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
     callback = function(args)
-        vim.lsp.enable("eslint", { bufnr = args.buf })
-        vim.lsp.enable("ts_ls", { bufnr = args.buf })
+        vim.lsp.enable("vtsls") 
     end,
 })
